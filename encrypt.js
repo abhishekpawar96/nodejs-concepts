1894a12ed3879dd41451defd8829eb10a81006bc
abhishekpawar96 <abhishekpawar96@gmail.com>
2020-04-25 01:07:03 +0530

    update encrypt

---
+++ encrypt.js
@@ -0,0 +1,19 @@
+const crypto = require('crypto')
+const fs = require('fs');
+const zlib = require('zlib');
+const { Transform } = require('stream');
+const file = process.argv[2];
+
+const progress = new Transform({
+    transform(chunk, encoding, callback) {
+        process.stdout.write('.');
+        callback(null, chunk);
+    }
+});
+
+fs.createReadStream(file)
+    .pipe(zlib.createGzip())
+    .pipe(crypto.createCipher('aes192', 'a_secret'))
+    .pipe(progress)
+    .pipe(fs.createWriteStream(file + '.gz'))
+    .on('finish', () => console.log('Done'));
